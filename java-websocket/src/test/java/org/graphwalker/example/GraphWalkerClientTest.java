b174a95c049b802c6fc737253b508243a454b030
Nils Olsson <nilols@spotify.com>
2014-11-28 13:52:21 +0100

    Update example project structure

--- java-websocket/src/test/java/org/graphwalker/example/GraphWalkerClientTest.java
+++ java-websocket/src/test/java/org/graphwalker/example/GraphWalkerClientTest.java
@@ -1,13 +1,11 @@
 package org.graphwalker.example;
 
-import org.junit.Test;
-
 import java.nio.file.Paths;
 
 public class GraphWalkerClientTest {
 
-    @Test
     public void smallModel() {
+
         GraphWalkerWebSocketClient c = new GraphWalkerWebSocketClient();
         c.run();
         c.loadModel(Paths.get("json/SmallModel.json"));
