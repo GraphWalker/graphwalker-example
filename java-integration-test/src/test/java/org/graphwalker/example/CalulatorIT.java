9e0f1b83f56498f713b9e0ed6246548c1ff5a39e
Nils Olsson <nilols@spotify.com>
2015-12-15 15:30:26 +0100

    Generate test report

--- java-integration-test/src/test/java/org/graphwalker/example/CalulatorIT.java
+++ java-integration-test/src/test/java/org/graphwalker/example/CalulatorIT.java
@@ -1,12 +1,23 @@
 package org.graphwalker.example;
 
+import org.graphwalker.java.test.Result;
+import org.graphwalker.java.test.TestExecutionException;
 import org.graphwalker.java.test.TestExecutor;
 import org.junit.Test;
+
+import java.io.File;
+import java.util.Date;
 
 public class CalulatorIT {
 
     @Test
     public void runCalculatorModel() throws Exception {
-        new TestExecutor(CalculatorTest.class).execute();
+        TestExecutor executor = new TestExecutor(CalculatorTest.class);
+        Date startTime = new Date();
+        Result result = executor.execute(false);
+        executor.reportResults(new File("target/reports"), startTime, System.getProperties());
+        if (result.hasErrors()) {
+            throw new TestExecutionException();
+        }
     }
 }
