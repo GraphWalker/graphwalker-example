6828a798ac0f70ee7e8900ecea76c04f509f7610
Nils Olsson <nilols@spotify.com>
2015-12-15 15:52:03 +0100

    Configure path generator and stop condition

--- java-integration-test/src/test/java/org/graphwalker/example/CalculatorTest.java
+++ java-integration-test/src/test/java/org/graphwalker/example/CalculatorTest.java
@@ -1,9 +1,11 @@
 package org.graphwalker.example;
 
+import org.graphwalker.core.condition.ReachedVertex;
+import org.graphwalker.core.generator.AStarPath;
 import org.graphwalker.core.machine.ExecutionContext;
 import org.graphwalker.java.annotation.GraphWalker;
 
-@GraphWalker
+@GraphWalker(pathGenerator = AStarPath.class, stopCondition = ReachedVertex.class, stopConditionValue = "verifyResult")
 public class CalculatorTest extends ExecutionContext implements CalculatorModel {
 
     @Override
