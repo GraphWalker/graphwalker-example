c18fd4e86fd0c85e990801fd4f7ac5426f8088ca
Kristian Karl <kristian.hermann.karl@gmail.com>
2019-09-19 08:20:30 +0200

    Added implemenatation of initial model

---
+++ prestashop/src/main/java/com/prestoshop/modelimplementation/PrestoShotImpl.java
@@ -0,0 +1,46 @@
+package com.prestoshop.modelimplementation;
+
+
+import com.prestashop.PrestoShop;
+import org.graphwalker.core.machine.ExecutionContext;
+import org.graphwalker.java.annotation.GraphWalker;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class PrestoShotImpl extends ExecutionContext implements PrestoShop {
+    @Override
+    public void v_ConfirmOrder() {
+
+    }
+
+    @Override
+    public void e_Checkout() {
+
+    }
+
+    @Override
+    public void v_Product() {
+
+    }
+
+    @Override
+    public void e_Select_Product() {
+
+    }
+
+    @Override
+    public void v_HomePage() {
+
+    }
+
+    @Override
+    public void e_Cart() {
+
+    }
+
+    @Override
+    public void v_Cart() {
+
+    }
+}
